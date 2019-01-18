e714195f269d1c30eff6019eb8fdaf34a59e5151
肖洒 <xiaos@songcw.com>
2019-01-18 09:41:09 +0800

    主首页和洗衣首页

---
+++ pages/laundryOrder/index/index.js
@@ -0,0 +1,85 @@
+// pages/laundryOrder/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    banner: {
+      imgUrls: [
+        'http://sit.fbs.songchejr.com/images/login-bg.png',
+        'http://sit.fbs.songchejr.com/images/login-bg.png'
+      ],
+      indicatorDots: true,
+      autoplay: true,
+      interval: 5000,
+      duration: 1000
+    },
+    activeFirst: 0,
+    activeSecond: 0,
+    count: 0
+  },
+
+  onChangeFirst() {
+
+  },
+  onChangeSecond() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
